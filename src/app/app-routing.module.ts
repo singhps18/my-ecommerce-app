38ca5b1c371fb30c5e52e164d92a6275caa894d8
singhps18 <singh1998priya18@gmail.com>
2024-08-11 01:15:53 +0530

    new api merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'homecom', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServiceComponent },
  
 ]
 
